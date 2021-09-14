755174c00d80334afd17422a540d6adeb3f94807
KelvinSu <1093792153@qq.com>
2021-09-14 20:36:06 +0800

    项目初始结构创建

---
+++ mall-order/src/test/java/com/kelvin/mall/morder/MallOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kelvin.mall.morder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
